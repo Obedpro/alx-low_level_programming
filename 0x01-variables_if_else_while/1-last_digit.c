1928aec3c4432ae1e837112d8cf6b0bea2a3c52c
Obedpro <sakora.patrick@gmail.com>
2022-09-10 09:29:57 -0700

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -36,15 +36,10 @@ int main(void)
 		{
 			printf("greater than 5\n");
 		}
-		else
+		else (n % 10 > (-5))
 		{
 			printf("less than 6 and not 0\n");
 		}
-
-		/**else if (n % 10 == 0)
-		{
-			printf("0\n");
-		}*/
 	}
 	return (0);
 }
