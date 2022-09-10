984d9138d79ed4ebda127ecf59187865b0606916
Obedpro <sakora.patrick@gmail.com>
2022-09-09 17:34:40 -0700

    Hello_printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main function
+* Description: using the main function 
 * this program prints with proper grammar, but the outcome is a piece of art,
 * Return: 0
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n"Ã);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
