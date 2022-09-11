072c6f8e26055a7279bdca5a17268a623d6b4623
Obedpro <sakora.patrick@gmail.com>
2022-09-11 06:52:32 -0700

    combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,14 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '8'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('9');
 	return (0);
 }
