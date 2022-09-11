b5b7fc51f3cae5b2d190f8f6b6bb493a685f7389
Obedpro <sakora.patrick@gmail.com>
2022-09-10 22:08:36 -0700

    Alphabet_inverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main prints alphabet in inverse
+*
+* Description: using the main function
+* this program prints lowercase alphabet in reverse
+* Return: 0
+*/
+int main(void)
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; d--)
+		putchar(d);
+	putchar('\n');
+	return (0);
+}
