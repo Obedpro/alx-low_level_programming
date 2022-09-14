64de1f6c37902a9fab022a08a8ab1f8662715c22
Obedpro <sakora.patrick@.com>
2022-09-14 07:14:00 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,11 +5,7 @@
  * @c: is the int that will use for the argument of the function
  * Return: 0
  */
-int _isalpha(int c){
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return (1);
-	}
-	else
-		return (0);
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 }
