70ecb8b951a2b328586b7d8508171518808eac03
Obedpro <sakora.patrick@.com>
2022-09-15 05:43:31 -0700

    More_Numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,13 @@
  */
 void more_numbers(void)
 {
-	int i;
-	int j = 0;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		while (j <= 14)
+		for (j = 0; j <= 14; j++)
 		{
 			_putchar(j + '0');
-			j++;
 		}
 		_putchar('\n');
 	}
