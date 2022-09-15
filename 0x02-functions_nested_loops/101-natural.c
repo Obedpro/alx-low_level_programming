0c56a1072ca82f2d5b45052803b75c9bc89b2104
Obedpro <sakora.patrick@.com>
2022-09-14 22:14:32 -0700

    Sum_3_5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,7 +10,6 @@ int main(void)
 {
 	int i = 0;
 	int n = 0;
-	int k;
 	int j = 0;
 	int r = 0;
 
@@ -24,7 +23,6 @@ int main(void)
 		r += (5  * j);
 		j ++;
 	}
-	k = n + r
-	printf("The sum of multiples of 3 or 5 bellow 1024 is %d\n", k);
+	printf("The sum of multiples of 3 or 5 bellow 1024 is %d\n", n + r);
 	return (0);
 }
