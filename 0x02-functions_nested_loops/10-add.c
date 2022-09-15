1a9dc11767e86970824f995b230d34958a67c883
Obedpro <sakora.patrick@.com>
2022-09-14 20:18:20 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @n: the two parameters
+ * @m: second integer
+ * Return: 0
+ */
+int add(int, int);
+{
+	int n, m;
+	int result;
+
+	result = n + m;
+	returm (result);
+}
