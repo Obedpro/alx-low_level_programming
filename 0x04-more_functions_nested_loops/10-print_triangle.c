7392ed77d7cda059b1fde44f62be0ee78fa39fec
Obedpro <sakora.patrick@.com>
2022-09-16 09:42:23 -0700

    Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,8 +17,8 @@ void print_triangle(int size)
 			for (j = i + 1; j < size; j++)
 				_putchar(' ');
 				for (j = 0; j <= i; j++)
-			       		_putchar('#');
+					_putchar('#');
 			_putchar('\n');
 		}
 	}
-}	
+}
