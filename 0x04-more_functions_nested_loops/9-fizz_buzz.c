85970ce8ef99c5efd3e1a35a374e3f76f2fca69b
Obedpro <sakora.patrick@.com>
2022-09-16 08:10:52 -0700

    Fizz_Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,23 +3,24 @@
 
 /**
  * fizzBuzz - print 100
- *Return: 0
+ *
  */
-int fizzBuzz(void)
+void fizzBuzz(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) != 0)
-			printf("Fizz ");
+			printf("Fizz, ");
 		else if ((i % 5) == 0 && (i % 3) != 0)
-			printf("Buzz ");
+			printf("Buzz, ");
 		else if (i % 15 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz, ");
 		else
-			printf("%d ", i);
+			printf("%d, ", i);
 	}
+	if (i == 100)
+		printf("Buzz");
 	printf("\n");
-	return (0);
 }
