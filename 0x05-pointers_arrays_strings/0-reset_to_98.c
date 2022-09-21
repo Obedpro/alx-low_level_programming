6e073df66236176ea074034a45c3e010629e1641
Obedpro <sakora.patrick@.com>
2022-09-20 19:21:50 -0700

    98_Battery

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,6 +3,7 @@
 /**
  * reset_to_98(int *n) - function that takes a pointer to an int
  * @n: the parameter in the function
+ * Description:  function that takes a pointer to an int as parameter and updates the value it points to to 98.
  * Return: 0
  */
 
